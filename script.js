da1685e627463740051aadc68ce02172cfd781d4
Maiga Boubacar <maiga@MacBook-Pro-de-Maiga.local>
2020-02-23 17:48:15 +0100

    add reset button

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 var colors = generateRandomColor(6);
-
 var square = document.getElementsByClassName("square");
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var reset = document.querySelector("#reset");
 var colorPicked = pickColor();
 
 colorDisplay.textContent = colorPicked;
+
+reset.addEventListener("click", function(){
+    // generate all new colors
+    colors = generateRandomColor(6);
+    // pick a new random color from array
+    colorPicked = pickColor();
+    // change colorDisplay to match picked Color
+    colorDisplay.textContent = colorPicked;
+    // change colors of squares
+    for(var i = 0; i < square.length; i++){
+        square[i].style.background = colors[i];
+    }
+    // reset background color
+    h1.style.background = "";
+    // reset button textcontent
+    reset.textContent = "New Colors";
+});
 
 
 for(var i = 0; i < square.length; i++){
@@ -22,6 +40,8 @@ for(var i = 0; i < square.length; i++){
         if (clickedColor === colorPicked){
            changeColors(colorPicked);
             messageDisplay.textContent = "Correct";
+            reset.textContent = "Play Again ?"
+            h1.style.background = clickedColor;
         } else {
             this.style.background = "#FDF2E9";
             messageDisplay.textContent = "Try Again";
