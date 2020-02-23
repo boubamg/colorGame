2ef150abb2eab934e03acbe58f91a9cc7343df7a
Maiga Boubacar <maiga@MacBook-Pro-de-Maiga.local>
2020-02-23 15:26:32 +0100

    init var, add event and if statement

--- script.js
+++ script.js
@@ -8,7 +8,21 @@ var colors = [
 ];
 
 var square = document.getElementsByClassName("square");
+var colorDisplay = document.getElementById("colorDisplay");
+var colorPicked = colors[2];
+
+colorDisplay.textContent = colorPicked;
+
 
 for(var i = 0; i < square.length; i++){
     square[i].style.background = colors[i];
+
+    square[i].addEventListener("click", function(){
+        
+        if (this.style.background === colorPicked){
+            alert("Win");
+        } else {
+            alert("loose");
+        }
+    });
 }
