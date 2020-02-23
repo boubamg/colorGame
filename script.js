0820fc782ea8d0396f96e28abb9d009e2e00780f
Maiga Boubacar <maiga@MacBook-Pro-de-Maiga.local>
2020-02-23 14:51:46 +0100

    add color with javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(255, 0, 255)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+];
+
+var square = document.getElementsByClassName("square");
+
+for(var i = 0; i < square.length; i++){
+    square[i].style.background = colors[i];
+}
